b9ef8be267149973a51a71dbc80c9677c3e4474d
Christina <skouloudi@gmail.com>
2016-11-29 17:44:33 +0200

    some drone MAC addresses
    MAC address for SZ DJI, Parrot, etc

--- detect_distance.py
+++ detect_distance.py
@@ -7,8 +7,8 @@ form_class = uic.loadUiType("tempconv.ui")[0]                 # Load the UI
 class MyWindowClass(QtGui.QMainWindow, form_class):
   def __init__(self, parent=None):
       # more code here
-
-
+      # MAC addresses: 60:60:1F, 90:03:B7, A0:14:3D, 00:12:1C, 00:26:7E
+   
 
 app = QtGui.QApplication(sys.argv)
 myWindow = MyWindowClass(None)
