ac0f82d3fb16cacf83aa5cf985b138c2fcd1e86c
Christina Skouloudi <skouloudi@gmail.com>
2016-11-29 22:32:27 +0200

    switched from pyqt to tkinter for gui

--- detect_distance.py
+++ detect_distance.py
@@ -1,16 +1,13 @@
+#!/usr/bin/python
+from tkinter import *
+from tkinter import messagebox
 
-import sys
-from PyQt4 import QtCore, QtGui, uic
- 
-form_class = uic.loadUiType("tempconv.ui")[0]                 # Load the UI
-
-class MyWindowClass(QtGui.QMainWindow, form_class):
-  def __init__(self, parent=None):
-      # more code here
-      # MAC addresses: 60:60:1F, 90:03:B7, A0:14:3D, 00:12:1C, 00:26:7E
-   
-
-app = QtGui.QApplication(sys.argv)
-myWindow = MyWindowClass(None)
-myWindow.show()
-app.exec_()
+top = Tk()
+top.geometry("200x200")
+# Code to add widgets will go here...
+def helloCallBack():
+    messagebox.showinfo( "Hello Python", "Hello World")
+    #MAC addresses: 60:60:1F, 90:03:B7, A0:14:3D, 00:12:1C, 00:26:7E
+screen = Button(top, text ="Hello", command = helloCallBack)
+screen.place(x=100, y=100)
+top.mainloop()
