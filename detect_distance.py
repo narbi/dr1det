4725f4430acfa322a43762f00925d4e4e13c7fc7
Christina Skouloudi <skouloudi@gmail.com>
2016-11-29 23:38:07 +0200

    os command to get bssids primitive distance calculation

--- detect_distance.py
+++ detect_distance.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python
 from tkinter import *
 from tkinter import messagebox
+import math
+import os
 
 top = Tk()
 top.geometry("200x200")
-# Code to add widgets will go here...
-def helloCallBack():
-    messagebox.showinfo( "Hello Python", "Hello World")
-    #MAC addresses: 60:60:1F, 90:03:B7, A0:14:3D, 00:12:1C, 00:26:7E
-screen = Button(top, text ="Hello", command = helloCallBack)
+
+def scanDrones():
+    mac_addresses = ["60:60:1F", "90:03:B7", "A0:14:3D", "00:12:1C", "00:26:7E"]
+
+    # add check for OS
+    scan = os.popen("netsh wlan show network mode=bssid").read()
+
+    #get frequency
+    frequency = 2412  #MHz
+
+    #get signal level
+    signalLevel = -57 #dbm
+
+    distance = math.pow(10,((27.55 - (20 * math.log10(frequency)) + signalLevel)/20))
+
+    messagebox.showinfo( "Searching..", distance)
+
+screen = Button(top, text ="start scanning", command = scanDrones)
+
 screen.place(x=100, y=100)
 top.mainloop()
