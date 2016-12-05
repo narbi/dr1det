b2c13f86764e014c9cbff8b31e783bb9ff8c2a96
Christina Skouloudi <skouloudi@gmail.com>
2016-12-06 01:04:17 +0200

    distance calculation changes and deauth commands

--- detect_distance.py
+++ detect_distance.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python
 from tkinter import *
 from tkinter import messagebox
+from tkinter import PhotoImage
+from PIL import Image,ImageTk
+import googlemaps
+import urllib
+import urllib.request
 import math
+import base64
 from sys import platform
 import os
 
 def scanDrones():
+    # 60:60:1F = DJI Phanton
+    # 90:03:B7, A0:14:3D, 00:12:1C, 00:26:7E = Parrot
+
     mac_addresses = ["60:60:1F", "90:03:B7", "A0:14:3D", "00:12:1C", "00:26:7E"]
 
     # check for cross platform functionality
@@ -13,29 +22,36 @@ def scanDrones():
         scan = os.popen("airodump-ng -d "+mac_addresses[0]+":00:00:00 -m FF:FF:FF:00:00:00 wlan0").read()
     elif platform == "darwin":
         # command to get APs for OS X
-        scan = ""
+        pass
     elif platform == "win32":
-        scan = os.popen("netsh wlan show network mode=bssid | finstr "+mac_addresses[0]+" Signal").read()
+        scan = os.popen("netsh wlan show network mode=bssid | findstr \""+mac_addresses[0]+" Signal\"").read()
 
-    ########
-    #dummy set of frequency (MHz)
-    frequency = 2412
     #dummy set of signal level (dbm)
     signalLevel = -57
-    ########
-    distance = get_distance(frequency, signalLevel)
-    messagebox.showinfo( "Drone detected", distance )
 
-def get_distance(freq, signal):
+    distance = get_distance(signalLevel)
+    messagebox.showinfo( "Drone detected", "Drone detected in "+str(distance)+" meters")
+
+def get_distance(signal):
     #calculate distance (m)
-    distance = math.pow(10,((27.55 - (20 * math.log10(freq)) + signal)/20))
+    frequency = 2412 #dummy set of frequency (MHz)
+    distance = math.pow(10,((27.55 - (20 * math.log10(frequency)) + signal)/20))
     return distance
 
+def deauth():
+    #airmon-ng start wlan0
+    #iwlist wlan0
+    # get the channel..
+    #airodump-ng mon0 -c <channel>  --bssid  <mac address of AP>
+    #iwconfig wlan0 channel 11
+    #iwconfig mon0 channel 11
+    #aireplay-ng --deauth 0 -a <mac address of AP> -c <mac address of client/victim> mon0
+    pass
 
 top = Tk()
 top.geometry("200x200")
 
-screen = Button(top, text ="Start scanning", command = scanDrones)
-screen.place(x=100, y=100)
+button = Button(top, text ="Start scanning", command = scanDrones)
+button.pack(side=TOP)
 
 top.mainloop()
