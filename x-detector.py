d2be9362eb22dc1ff2840b2c40fbf80b6dd7737a
Christina Skouloudi <skouloudi@gmail.com>
2017-01-23 23:18:14 +0200

    kivy init with mapview

--- x-detector.py
+++ x-detector.py
@@ -1,8 +1,18 @@
+from kivy.garden.mapview import MapView
 from kivy.app import App
-
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.image import Image
 
 class DetectingDrones(App):
-  pass
+  def build(self):
+    layout = BoxLayout(orientation='vertical')
+    mapview = MapView(zoom=15, lat=38.0503249, lon=23.8064504)
+    layout.add_widget(mapview)
+    button = Button(text='scan drones')
+    layout.add_widget(button)
+    return layout
+
   
 if __name__ == "__main__":
   DetectingDrones().run()
