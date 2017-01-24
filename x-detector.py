cd849f30b2d0f02be0c1b94def9f1a91657b454b
Christina Skouloudi <skouloudi@gmail.com>
2017-01-25 00:30:13 +0200

    kivy init wchange of screens mapview on screen one result drone image on screen two

--- x-detector.py
+++ x-detector.py
@@ -3,16 +3,24 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.image import Image
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.properties import ObjectProperty, NumericProperty
 
-class DetectingDrones(App):
+class ScreenTwo(Screen):
+  pass
+
+class ScreenOne(Screen):
+  mapview = MapView()
+
+
+class Manager(ScreenManager):
+  main_screen = ObjectProperty(None)
+  results_screen = ObjectProperty(None)
+
+class MainApp(App):
   def build(self):
-    layout = BoxLayout(orientation='vertical')
-    mapview = MapView(zoom=15, lat=38.0503249, lon=23.8064504)
-    layout.add_widget(mapview)
-    button = Button(text='scan drones')
-    layout.add_widget(button)
-    return layout
-
+    m = Manager(transition=NoTransition())
+    return m
   
 if __name__ == "__main__":
-  DetectingDrones().run()
+  MainApp().run()
